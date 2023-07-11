c7f1f51f605f50d239ef132672cb81ed35e3a96b
OPaslavskiy <bankir.op@gmail.com>
2023-07-11 13:20:08 +0300

    add post

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,17 +1,38 @@
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
 
 type Props = {
-    params: {
-        id: string
+  params: {
+    id: string;
+  };
+};
+
+async function getData(id: string) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
     }
+  );
+
+  return response.json();
 }
 
-export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
-    return {
+export async function generateMetadata({
+  params: { id },
+}: Props): Promise<Metadata> {
+  return {
     title: id,
-    }
+  };
 }
 
-export default function Post({params: {id}}: Props) {
-    return <h1>Post page { id }</h1>
+export default async function Post({ params: { id } }: Props) {
+  const post = await getData(id);
+  return (
+    <>
+      <h1 className="container">{post.title}</h1>
+      <p className="container">{post.body}</p>
+    </>
+  );
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,32 @@
-import type { Metadata } from 'next'
-export const metadata: Metadata = {
-  title: 'Blog',
- 
+import type { Metadata } from "next";
+import Link from "next/link"
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60
+    }
+  });
+
+  return response.json();
 }
 
-export default function Blog() {
-    return <h1>Blog page</h1>
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+};
+
+export default async function Blog() {
+  const posts = await getData();
+  return (
+    <>
+      <h1>Blog page</h1>
+      <ul className="container">
+        {posts.map((post: any) => (
+          <li key={post.is}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
