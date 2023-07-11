21cc07ff07434f97fd3b538cd953f7df0c3b5af4
OPaslavskiy <bankir.op@gmail.com>
2023-07-11 12:55:37 +0300

    add metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'About | Next App',
+ 
+}
+
 export default function About() {
     return <h3>Select subitem</h3>
 }
