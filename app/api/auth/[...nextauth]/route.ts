dc7fc97d4ec9c8924d4f41464fb8753e81db2b9d
OPaslavskiy <bankir.op@gmail.com>
2023-07-13 15:19:41 +0300

    googleAuth  are working

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,4 +3,5 @@ import NextAuth from 'next-auth'
 
 const handler = NextAuth(authConfig);
 
+
 export { handler as GET, handler as POST }
--- configs/auth.ts
+++ configs/auth.ts
@@ -2,10 +2,12 @@ import type {AuthOptions} from 'next-auth';
 import GoggleProvider from 'next-auth/providers/google'
 
 export const authConfig: AuthOptions = {
+    
+    
     providers: [
         GoggleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
           })
     ]
 };
